69687c0cc80a082dcc2e66aebc456b5de7499586
komi <pashaandreev66666@gmail.com>
2025-09-28 21:39:33 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 int main() {
   double a = 5.54;
@@ -12,18 +13,17 @@ int main() {
   double gamma = 1.45; 
   double T1 = 288; 
   double T2 = 675; 
-  
-  double K = (gamma - 1) / gamma;
+  double K;
+
+  K = (gamma - 1) / gamma;
  
   double eta1 = 1 - pow(1 / b, K);// Расчет КПД для цикла из двух изобар и двух адиабат
-
   double eta2 = (T2 - T1) / (T2 + (T2 - T1) / ((gamma - 1) * log(a)));// Расчет КПД для цикла из двух изохор и двух изотерм
-
   double eta3 = 1 - K * log(b) / (pow(b, K) - 1);// Расчет КПД для цикла из изотермы и адиабаты с изотермой
 
-  std::cout << "КПД для цикла из двух изобар и двух адиабат:      " << eta1 << std::endl;
-  std::cout << "КПД для цикла из двух изохор и двух изотерм:      " << eta2 << std::endl;
-  std::cout << "КПД для цикла из изотермы и адиабаты с изотермой: " << eta3 << std::endl;
+  cout << "Efficiency for a cycle with two isobars and two adiabats:      " << eta1 << endl
+       << "Efficiency for a cycle of two isochores and two isotherms:     " << eta2 << endl
+       << "Efficiency for a cycle of isotherm and adiabat with isotherm:  " << eta3 << endl;
 
   return 0;
 }
